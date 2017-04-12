a25840c4a64e33e7d41424c706b1c10e707d8a9e
DmitrySmetyukhov <dmitry.smetyukhov@gmail.com>
2017-04-12 22:21:11 +0300

    last

--- export1.js
+++ export1.js
@@ -1,37 +1,32 @@
 let FS = require("fs")
 let MongoDB = require("mongodb")
 let MongoClient = MongoDB.MongoClient
-let {Readable} = require("stream")
 let {Stringifier} = require("newline-json")
-let stringifier = new Stringifier()
+
+let scheme = "scheme.json"
 let [dbName] = process.argv.slice(2)
 let url = "mongodb://localhost:27017/" + dbName
 
-  ;
-(async() => {
-  try {
-    var db = await MongoClient.connect(url)
-  } catch (err) {
+MongoClient.connect(url, (err, db) => {
+  if (err) {
     console.log("Unable to connect to the mongoDB server. Error:", err)
     process.exit(1)
   }
-
   console.log("Connection established to", url)
 
   ;(async() => {
-    // TODO write scheme.json
-
     let collectionNames = (await db.listCollections().toArray()).map(x => x.name)
+    let file = FS.createWriteStream(scheme)
+    await file.write(JSON.stringify(collectionNames));
     for (let collectionName of collectionNames) {
       let collection = db.collection(collectionName)
       await new Promise((resolve) => {
         let file = FS.createWriteStream(collectionName + ".log")
         file.on("finish", resolve)
-        collection.find().pipe(stringifier).pipe(file)
+        collection.find().pipe(new Stringifier()).pipe(file)
       })
     }
   })().then(db.close.bind(db))
-
-})()
+})
 
 
--- import1.js
+++ import1.js
@@ -1,41 +1,45 @@
-"use strict"
-let FS = require('fs')
-let JSONStream = require('JSONStream')
-let mongodb = require('mongodb')
-let MongoClient = mongodb.MongoClient
+let FS = require("fs")
 let {Readable} = require("stream")
-let {Stringifier, Parser} = require("newline-json")
+let JSONStream = require('JSONStream')
+let MongoDB = require("mongodb")
+let MongoClient = MongoDB.MongoClient
+let {Parser} = require("newline-json")
 
-
-let stringifier = new Stringifier()
+let scheme = "scheme.json"
 let parser = new Parser()
-
-
-let dbName = process.argv.slice(2) // [dbName]
-
+let [dbName] = process.argv.slice(2)
 let url = 'mongodb://localhost:27017/' + dbName
 
-MongoClient.connect(url, function (err, db) {
-
+MongoClient.connect(url, (err, db) => {
   if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err)
-  } else {
-
-    console.log('Connection established to', url);
-
-    ;(async() => {
-      // TODO take collectionNames from scheme.json
-      let collectionNames = (await db.listCollections().toArray()).map(x => x.name)
-
-      for (let collectionName of collectionNames) {
-        let collection = db.collection(collectionName)
-        await collection.remove();
-        await new Promise((resolve) => {
-          let file = FS.createReadStream(collectionName + ".log")
-          file.pipe(parser).on("data", d => collection.insert(d))
-          file.on("end", resolve)
-        })
-      }
-    })().then(db.close.bind(db))
+    console.log("Unable to connect to the mongoDB server. Error:", err)
+    process.exit(1)
   }
+
+  console.log("Connection established to", url)
+
+  ;(async() => {
+    // TODO take collectionNames from scheme.json
+
+    let collectionNames;
+    await new Promise((resolve) => {
+      let file = FS.createReadStream(scheme, {flags: 'r', encoding: 'utf-8'})
+      file.pipe(parser).on("data", d => collectionNames = d)
+      file.on("end", resolve)
+    })
+
+    await db.dropDatabase()
+    for (let collectionName of collectionNames) {
+      let collection = db.collection(collectionName)
+      await collection.remove()
+      await new Promise((resolve) => {
+        let file = FS.createReadStream(collectionName + ".log", {flags: 'r', encoding: 'utf-8'})
+        file.pipe(new Parser()).on("readable", () => {
+          file.read()
+        })
+        file.on("data", d => collection.insert(d))
+        file.on("end", resolve)
+      })
+    }
+  })().then(db.close.bind(db))
 })
